23a455b2664666b4ae16fc81943c9a847dac2662
bokdol11859 <2019147551@yonsei.ac.kr>
2022-10-11 01:43:22 +0900

    feat: section buttons created

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -22,7 +22,7 @@ const BackgroundCircles = (props: Props) => {
       <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52" />
       <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52" />
       <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52" />
-      <div className="absolute rounded-full border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
+      <div className="absolute rounded-full border border-[#51abcb] opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
       <div className="absolute border border-[#333333] rounded-full h-[800px] w-[800px] mt-52" />
     </motion.div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -8,7 +9,6 @@ const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
     words: [
       "Hi, My Name is Eric Park",
-      "A Passionate Software Engineer",
       "npx create-next-app --typescript",
       "python manage.py runserver",
       "#include <bits/stdc++.h>",
@@ -20,10 +20,34 @@ const Hero = (props: Props) => {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="/logo.png"
+        alt="logo"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="#51abcb" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
