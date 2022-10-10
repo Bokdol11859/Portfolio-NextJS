c949ed7744d9f4ced1094ffabee4025a60047be2
bokdol11859 <2019147551@yonsei.ac.kr>
2022-10-10 23:55:46 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>박준열 - 프론트엔드 개발자</title>
+      </Head>
+
+      {/* header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+    </div>
+  );
+};
+
+export default Home;
