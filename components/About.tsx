9f6e83a2bf65f21669c85c5be20886d65d35c096
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-10-11 18:39:06 +0900

    feat: about section created

---
+++ components/About.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute ml-6 top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{
+          duration: 0.8,
+        }}
+        // viewport={{ once: true }}
+        src="/image.jpeg"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-[300px] md:h-[400px] xl:w-[500px] xl:h-[600px]"
+      />
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          &quot;개발을{" "}
+          <span className="underline underline-offset-8 decoration-[#51abcb]/50">
+            즐기는
+          </span>{" "}
+          개발자&quot;
+        </h4>
+        <p className="text-sm">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).It is a
+          long established fact that a reader will be distracted by the readable
+          content of a page w
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default About;
