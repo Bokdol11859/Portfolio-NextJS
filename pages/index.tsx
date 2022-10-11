7ac7034d9a3476128183764b9ffe17d0c1b5776c
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-10-11 19:49:02 +0900

    feat: skills section finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import About from "../components/About";
 import Experience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Skills from "../components/Skills";
 
 const Home: NextPage = () => {
   return (
@@ -32,7 +33,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* Skills */}
-
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
       {/* Projects */}
 
       {/* Contact Me */}
