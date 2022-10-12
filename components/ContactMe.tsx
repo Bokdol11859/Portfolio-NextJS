9c99105eb0f91b47aaf1aec7ca789901172cc0c6
bokdol11859 <2019147551@yonsei.ac.kr>
2022-10-12 21:30:08 +0900

    feat: contact me mail href created

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -5,10 +5,23 @@ import {
   EnvelopeIcon,
   BellIcon,
 } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  number: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 const ContactMe = (props: Props) => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:eric010506@naver.com?subject=${formData.subject}&body=안녕하세요 제 이름은 ${formData.name}입니다. ${formData.message} ${formData.number}로 연락 주세요.`;
+  };
+
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-4xl px-10 justify-evenly mx-auto items-center overflow-x-hidden">
       <h3 className="absolute ml-6 top-20 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -35,13 +48,35 @@ const ContactMe = (props: Props) => {
             <p className="text-2xl lg:text-4xl">대한민국 서초구 잠원동</p>
           </div>
         </div>
-        <form className="flex flex-col space-y-2 w-fit mx-auto ">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto "
+        >
           <div className="flex space-x-2">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="Email" className="contactInput" type="email" />
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("number")}
+              placeholder="Telephone"
+              className="contactInput"
+              type="tel"
+            />
           </div>
-          <input placeholder="Title" className="contactInput" type="text" />
-          <textarea placeholder="Message" className="contactInput" />
+          <input
+            {...register("subject")}
+            placeholder="Title"
+            className="contactInput"
+            type="text"
+          />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
           <button
             type="submit"
             className="bg-[#51abcb] py-5 px-10 rounded-md text-black font-bold text-lg relative"
